98f1bf93131b73d209b6bbc8685cbd0da14e5712
Mubarak-1998 <mubarak.alqnaam@gmail.com>
2022-08-12 21:59:39 +0300

    Initial commit

---
+++ lib/views/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:movie_db/services/remote_service.dart';
+
+import '../models/post.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Post>? posts;
+  var isLoaded = false;
+
+  @override
+  void initState(){
+    super.initState();
+
+
+getData();
+
+  }
+
+
+  getData() async{
+    posts = await RemoteService().getPosts();
+    if (posts != null){
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Posts'),
+        ),
+        body: Visibility(
+          visible: isLoaded,
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+          child: ListView.builder(
+              itemCount: posts?.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Text(posts![index].title),
+                );
+              }),
+        ));
+  }
+}
